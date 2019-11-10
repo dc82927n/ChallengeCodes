17274145c6936468fb89c6a9a6de1f80dfceba3a
David <dc82927n@gmail.com>
2019-11-10 16:49:47 -0500

    new leetcode 111019

---
+++ FALL2019/LeetCode/961-N-RepeatedElement2Narray.js
@@ -0,0 +1,56 @@
+/*961. N-Repeated Element in Size 2N Array
+
+In a array A of size 2N, there are N+1 unique elements, 
+and exactly one of these elements is repeated N times.
+
+Return the element repeated N times.
+
+ 
+
+Example 1:
+
+Input: [1,2,3,3]
+Output: 3
+
+Example 2:
+
+Input: [2,1,2,5,3,2]
+Output: 2
+
+Example 3:
+
+Input: [5,1,5,2,5,3,5,4]
+Output: 5
+
+ 
+
+Note:
+
+    4 <= A.length <= 10000
+    0 <= A[i] < 10000
+    A.length is even
+
+*/
+
+
+// objective is to count the output of repeated elements in the array. 
+
+var repeatedNTimes = function(A) {
+
+    var count = 0; 
+    
+    for (var i = 0; i < A.length; i++){
+        for (var j = i+1; j < A.length; j++){
+            if(A[i] === A[j]){ 
+                return A[j]
+            } 
+            
+        }
+     
+    } 
+      
+        return 0 
+    
+};
+
+console.log(repeatedNTimes([1,2,3,3]))
